881824e19dfdd81051a72299c1186a0f44e8fa09
Jack Barnett <jsbarnett@berkeley.edu>
2016-09-21 12:27:02 -0700

    COMPLETED HW1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,33 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+    # Maps from strings to ints
+    a = a.map { |i| i.to_i }
+
+    # Adds 2 to each int
+    a.map! do |b|
+        b += 2
+    end
+
+    # Deletes int if not even
+    a.delete_if { |b| b % 2 != 0}
+
+    # Makes a new array of unique ints
+    arr = []
+    a.each do |e|
+     arr << e unless arr.include?(e)
+    end
+
+    # Deletes ints greater than 10
+    arr.delete_if { |b| b > 10}
+
+    # Adds the final ints together & returns
+    c = 0
+    arr.map do |b|
+        c += b
+    end
+    return c
   end
 
   def foo(b)
@@ -15,5 +42,3 @@ class Foobar
     # Call with foobar_instance.bar
   end
 end
-
-
